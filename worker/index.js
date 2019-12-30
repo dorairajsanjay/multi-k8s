6cc04fe6c10684c0690028ca1dc85a5ad05857a9
Sanjay Dorairaj <sanjaydorairaj@Sanjays-MBP.hsd1.ca.comcast.net>
2019-12-30 14:42:33 -0800

    adding gitignore

--- worker/index.js
+++ worker/index.js
@@ -11,12 +11,17 @@ const redisClient = redis.createClient({
 const sub = redisClient.duplicate();
 
 function fib(index) {
+
+  console.log("Worker node in fib function executing for index:" + index);
+
   if (index < 2) return 1;
   return fib(index - 1) + fib(index - 2);
 }
 
 sub.on('message', (channel, message) => {
+  console.log("Worker node received a message of some sort:" + message);
   redisClient.hset('values', message, fib(parseInt(message)));
 });
 
+console.log("Worker node subscribing for inserts on REDIS");
 sub.subscribe('insert');
